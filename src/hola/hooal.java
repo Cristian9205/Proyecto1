1d80afce14179d10c2c7c25b09932cde938669f5
Cristian9205 <0046cgarces@e-itaca.es>
2024-02-15 19:20:03 +0100

    d

--- src/hola/hooal.java
+++ src/hola/hooal.java
@@ -3,7 +3,12 @@ package hola;
 public class hooal {
 
 	public static void main(String[] args) {
-
+		int num = 5;
+		for (int i = 1 ; i < num +1 ; i++) {
+			
+			System.out.println(i);
+		}
+		
 	}
 
 }
